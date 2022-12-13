165d7ec051a9c4665144f2e1e6beeb6e6a60a26e
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-12-13 18:04:35 -0500

    Capstone in progress

--- sneaker_app/models.py
+++ sneaker_app/models.py
@@ -9,7 +9,8 @@ class Sneaker(models.Model):
     brand = models.CharField(max_length=200)
     color = models.CharField(max_length=200, default='Not Available')
     price = models.IntegerField(default=0)
-    image = models.URLField(max_length=200, default='https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3cf0c669b07048cdb455ae02000e88df_9366/NMD_R1_Shoes_Pink_GZ4963_01_standard.jpg')
+    # image = models.URLField(max_length=200, default='https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3cf0c669b07048cdb455ae02000e88df_9366/NMD_R1_Shoes_Pink_GZ4963_01_standard.jpg')
+    image = models.JSONField(blank=True, null=True)
     links = models.URLField(max_length=200, default='https://stockx.com/nike-dunk-low-mineral-teal-gs')
     gender = models.CharField(max_length=200, default='N/A')
 
